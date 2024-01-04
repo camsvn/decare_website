6beae228d3e902b968d4c4096cd86f85179071e7
Amal Salvin Joseph <amalsalvin97@gmail.com>
2024-01-05 00:38:51 +0530

    chore: remove prefetch plugin

--- astro.config.ts
+++ astro.config.ts
@@ -2,13 +2,12 @@ import { defineConfig, sharpImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
 import remarkUnwrapImages from "remark-unwrap-images";
 
 // https://astro.build/config
 export default defineConfig({
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://astro-theme-cactus.netlify.app/",
+	site: "https://decare.team/",
 	markdown: {
 		remarkPlugins: [remarkUnwrapImages],
 		shikiConfig: {
@@ -23,13 +22,13 @@ export default defineConfig({
 		// https://docs.astro.build/en/guides/assets/#using-sharp
 		service: sharpImageService(),
 	},
+	prefetch: true,
 	integrations: [
 		mdx({}),
 		tailwind({
 			applyBaseStyles: false,
 		}),
 		sitemap(),
-		prefetch(),
 	],
 	compressHTML: true,
 	vite: {
